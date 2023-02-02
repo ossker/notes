eaab88e9b15f0d644bdfae2fc9571e3a2391b48f
ossker <oskar.bienko@onet.pl>
2023-02-03 00:44:47 +0100

    Django Rest Framework configured. Note serializer added. Get note by id and get all notes functions created.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -34,4 +38,16 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
